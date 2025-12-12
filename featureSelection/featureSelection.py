0026d3212f65a3cf498ea988127509d02149578e
jack-kolenbrander <jackkolenbrander@vt.edu>
2025-12-11 23:49:51 -0500

    Updated exclusions

--- featureSelection/featureSelection.py
+++ featureSelection/featureSelection.py
@@ -15,10 +15,25 @@ def initalize_modbus_dataset(dataset):
     df = dataset.copy()
 
     EXCLUSIONS = [
-        'IP_SRC', 'IP_DST',
-        'Ether_src', 'Ether_dst',
-
-        
+        # IP and MAC addresses
+        'IP_src', #IP source address 
+        'IP_dst', #IP Destination address
+        'Ether_src', # Source ethernet address
+        'Ether_dst', # Destination ethernet address
+        # Protocol values
+        'Ether_type', # Field that identifies upper-layer protcol or payload type
+        'IP_version', # Field that identifies IP version
+        'IP_proto', # Field that identifies protocol (TCP, UDP, ICMP, etc)
+        'IP_chksum', # IP layer checksum
+        'TCP_sport', # TCP source port
+        'TCP_dport', # TCP Destination port
+        'TCP_seq', # TCP sequence number
+        'TCP_ack', # Expected TCP sequence umber
+        'TCP_dataofs', # TCP offest 
+        'TCP_reserved', # TCP reserved field
+        'TCP_chksum', # TCP Checksum
+        'TCP_urgptr', # Used when urgent bit has been set
+        'TCP_options', # TCP optional field
     ]
 
     
